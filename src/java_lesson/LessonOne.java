26c7a5e07fcfc5dbe7fbc7164f7a1606cd82ed95
guitarbien <guitarbien@gmail.com>
2017-04-25 21:14:28 +0800

    create LessonOne.java

---
+++ src/java_lesson/LessonOne.java
@@ -0,0 +1,19 @@
+package java_lesson;
+
+public class LessonOne {
+	/*
+	 	模擬1粒骰子投擲20次(隨機)，並統計點數總和出現的次數, 將結果以數字印出. 
+   		例: 執行結束, 列出結果為: 
+   		1 :3次
+   		2 :8次
+   		3 :6次
+   		4 :3次
+   		5 :3次
+		6 :3次
+	 */
+	public static void main(String[] args) {
+
+
+	}
+
+}
