3619f88b969c917400d76bf974ff60944bf9cccf
guitarbien <guitarbien@gmail.com>
2017-04-26 00:17:03 +0800

    create LessonFour

---
+++ src/java_lesson/LessonFour.java
@@ -0,0 +1,14 @@
+package java_lesson;
+
+public class LessonFour {
+	/*
+	 * 隨機產生15個介於1-10之間的BigDecimal(不能重覆), 將其取至小數第三位(四捨五入)並依序存入Collection類物件,
+	 * 並進行下列計算: (1). 印出其排序後之數列 (2). 計算15個數字之平均值 例: 排序後: 1.009, 3.412, 3.985,
+	 * 4.500, 4.593, 5.002, 6.651, 7.909, 9.557, 9.881 平均數: 5.650
+	 * 
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
