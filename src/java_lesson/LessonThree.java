45e1afa387f1c555f34201607bcdc01a6c7c3a3c
guitarbien <guitarbien@gmail.com>
2017-04-26 00:03:41 +0800

    craete LessonThree

---
+++ src/java_lesson/LessonThree.java
@@ -0,0 +1,12 @@
+package java_lesson;
+
+public class LessonThree {
+	/*
+	 * 模擬3粒骰子投擲100次(隨機)，並統計點數總和出現的次數, 將結果以*印出. 請改以Map類物件來撰寫。(次數為0的不用印出) 例: 執行結束,
+	 * 列出結果為: 3 *** 4 ***
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
