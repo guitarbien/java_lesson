9b8e174d0bbfabe246ac76983a203f41069aa6cd
guitarbien <guitarbien@gmail.com>
2017-04-26 00:02:22 +0800

    用[]如何判斷是否重複 contains

--- src/java_lesson/LessonTwo.java
+++ src/java_lesson/LessonTwo.java
@@ -1,5 +1,7 @@
 package java_lesson;
 
+import java.text.DecimalFormat;
+
 public class LessonTwo {
 	/*
 	 * 隨機產生15個介於1-10之間的浮點數(不能重覆), 將其取至小數第三位並依序存入陣列, 並計算15個數字之總合與平均數 例: 陣列:
@@ -7,8 +9,31 @@ public class LessonTwo {
 	 * 56.5 平均數: 5.650
 	 */
 	public static void main(String[] args) {
+		String[] number = new String[15];
+		DecimalFormat df = new DecimalFormat("#.000");
 
+		for (int i = 0; i < 15; i++) {
+			String current = df.format(Math.random() * 9 + 1);
 
+//			for (int j = 0; j < number.length; j++) {
+//				if (null != number[j] && !number[j].equals(current)){
+//					i--;
+//					break;
+////					System.out.println('A' + j);
+//				}
+//			}
+			
+			number[i] = current;
+		}
+
+		double sum = 0;
+		for (String str : number) {
+			sum += Double.parseDouble(str);
+		}
+
+		System.out.println("陣列：" + String.join(", ", number));
+		System.out.println("總和：" + df.format(sum));
+		System.out.println("平均數：" + df.format(sum/15));
 	}
 
 }
