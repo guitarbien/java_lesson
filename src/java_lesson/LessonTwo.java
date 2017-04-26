64b6d7244a77e112cb6944d2b0a2f02b39db4a2c
guitarbien <guitarbien@gmail.com>
2017-04-26 19:54:13 +0800

    fix syntax error

--- src/java_lesson/LessonTwo.java
+++ src/java_lesson/LessonTwo.java
@@ -3,30 +3,31 @@ package java_lesson;
 import java.text.DecimalFormat;
 
 public class LessonTwo {
-		String[] number = new String[ 15 ];
-		DecimalFormat df = new DecimalFormat( "#.000" );
+	public static void main() {
+		String[] number = new String[15];
+		 DecimalFormat df = new DecimalFormat( "#.000" );
 
-		for ( int i = 0; i < 15; i++ ) {
-			String current = df.format( Math.random() * 9 + 1 );
+		for (int i = 0; i < 15; i++) {
+			String current = df.format(Math.random() * 9 + 1);
 
-			for ( int j = 0; j < i; j++ ) {
-				if ( number[ j ].equals( current ) ) {
+			for (int j = 0; j < i; j++) {
+				if (number[j].equals(current)) {
 					i--;
 					break;
 				}
 			}
 
-			number[ i ] = current;
+			number[i] = current;
 		}
 
 		double sum = 0;
-		for ( String str : number ) {
-			sum += Double.parseDouble( str );
+		for (String str : number) {
+			sum += Double.parseDouble(str);
 		}
 
-		System.out.println( "陣列：" + String.join( ", ", number ) );
-		System.out.println( "總和：" + df.format( sum ) );
-		System.out.println( "平均數：" + df.format( sum / 15 ) );
+		System.out.println("陣列：" + String.join(", ", number));
+		System.out.println("總和：" + df.format(sum));
+		System.out.println("平均數：" + df.format(sum / 15));
 	}
 
 }
