21f0e89d4cd84baa721a3e67c723bed52a753bd1
Ricky Bien <guitarbien@gmail.com>
2017-04-27 18:45:48 +0800

    Update LessonEight.java

--- src/java_lesson/LessonEight.java
+++ src/java_lesson/LessonEight.java
@@ -4,63 +4,157 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class LessonEight {
 	/*
-	 * 一筆資料轉存成一個Map，並將所有資料放入List中並 利用Collections類別的sort方法，針對Price這個欄位進行
-	 * 資料排序後輸出成另一份檔案, ex: cars2.csv 。
-	 * 
-	 * 例如： Manufacturer TYPE Min.PRICE Price Cadillac Midsize 37.5 40.1
-	 * Chevrolet Sporty 34.6 38 Audi Midsize 30.8 37.7 ….
-	 * 
-	 * 
-	 * 針對Manufacturer這個欄位將資料分組，並計算Min.Price ,Price這兩個欄位的小計、合計並輸出在螢幕上。 例如：
-	 * Manufacturer TYPE Min.PRICE Price Audi Midsize 30.8 37.7 Audi Midsize
-	 * 25.9 29.1 小計 56.7 66.8 ……
-	 * 
-	 * 合計 所有資料的合計值 所有資料的合計值
+	 * 一筆資料轉存成一個Map，並將所有資料放入List中並 利用Collections類別的sort方法，針對Price這個欄位進行 資料排序後輸出成另一份檔案, ex: cars2.csv 。 例如： Manufacturer TYPE Min.PRICE Price Cadillac Midsize 37.5 40.1 Chevrolet Sporty 34.6 38 Audi
+	 * Midsize 30.8 37.7 …. 針對Manufacturer這個欄位將資料分組，並計算Min.Price ,Price這兩個欄位的小計、合計並輸出在螢幕上。 例如： Manufacturer TYPE Min.PRICE Price Audi Midsize 30.8 37.7 Audi Midsize 25.9 29.1 小計 56.7 66.8 …… 合計
+	 * 所有資料的合計值 所有資料的合計值
 	 */
-	public static void main(String[] args) {
+	private List<Car> cars;
+
+	private List<String> title;
+
+	public static void one( LessonEight lessonEight ) {
+		// -------------------
+		// sort by price asc
+		// -------------------
+		List<Car> cars = lessonEight.getCars();
+		cars.sort( ( c1, c2 ) -> Math.round( c1.getPrice() - c2.getPrice() ) );
+
+		// ---------------
+		// build result
+		// ---------------
+		StringBuffer stringBuffer = new StringBuffer();
+
+		stringBuffer.append( String.join( ",", lessonEight.getTitle() ) );
+		stringBuffer.append( "\r\n" );
+
+		for ( int i = 0; i < lessonEight.getCars().size(); i++ ) {
+			stringBuffer.append( lessonEight.getCars().get( i ).getManufacturer() );
+			stringBuffer.append( "," );
+			stringBuffer.append( lessonEight.getCars().get( i ).getType() );
+			stringBuffer.append( "," );
+			stringBuffer.append( lessonEight.getCars().get( i ).getMinPrice() );
+			stringBuffer.append( "," );
+			stringBuffer.append( lessonEight.getCars().get( i ).getPrice() );
+			stringBuffer.append( "\r\n" );
+		}
+
+		// ---------------
+		// save new file
+		// ---------------
+//		LessonSeven.writeFile( "D:\\cars2.csv", stringBuffer.toString() );
+		System.out.println( stringBuffer.toString() );
+		System.out.println( "output cars2.csv done" );
+	}
+
+	public static void two( LessonEight lessonEight ) {
+		// -------------------
+		// sort by price asc
+		// -------------------
+		List<Car> cars = lessonEight.getCars();
+
+		Map<String, List<Car>> group = cars.stream().collect( Collectors.groupingBy( Car::getManufacturer ) );
+		 
+
+//		cars.stream().forEach( car -> {
+//			System.out.println( car.getManufacturer() );
+//		} );
+
+//		Collections.sort( cars, new Comparator<Car>() {
+//			@Override
+//			public int compare( Car c1, Car c2 ) {
+//				int res = String.CASE_INSENSITIVE_ORDER.compare( c1.getManufacturer(), c2.getManufacturer() );
+//				if ( res == 0 ) {
+//					res = c1.getManufacturer().compareTo( c2.getManufacturer() );
+//				}
+//				return res;
+//			}
+//		} );
+//
+//		// ----------------------
+//		// Group by Manufacturer
+//		// ----------------------
+//		StringBuffer stringBuffer = new StringBuffer();
+//		System.out.println( String.join( ",", lessonEight.getTitle() ) );
+//
+//		Boolean sameGroup = true;
+//		String lastManufacturer = "";
+//		for ( int i = 0; i < lessonEight.getCars().size(); i++ ) {
+////			System.out.println( lessonEight.getCars().get( i ) );
+//			String currentManufacturer = lessonEight.getCars().get( i ).getManufacturer();
+//
+//			stringBuffer.append( lessonEight.getCars().get( i ).getManufacturer() );
+//			stringBuffer.append( "," );
+//			stringBuffer.append( lessonEight.getCars().get( i ).getType() );
+//			stringBuffer.append( "," );
+//			stringBuffer.append( lessonEight.getCars().get( i ).getMinPrice() );
+//			stringBuffer.append( "," );
+//			stringBuffer.append( lessonEight.getCars().get( i ).getPrice() );
+//			stringBuffer.append( "\r\n" );
+//
+//			if ( !lastManufacturer.equals( currentManufacturer ) ) {
+//				// 輸出小計
+//			}
+//
+//			currentManufacturer = lastManufacturer;
+//		}
+	}
+
+	public static void main( String[] args ) {
+
+		LessonEight lessonEight = new LessonEight();
+
+		lessonEight.readCarsData( "D:\\cars.csv" );
+
+//		one(lessonEight);
+		two( lessonEight );
+	}
+
+	public void readCarsData( String filepath ) {
+		ArrayList<Car> cars = new ArrayList<Car>();
+
 		try {
 			// read csv file each line into a Map
-			Scanner scan = new Scanner(new File("/Users/bien/Downloads/＜新進人員作業＞/cars.csv"));
+			Scanner scan = new Scanner( new File( filepath ) );
 
 			int lineCnt = 0;
-			List<String> title = new ArrayList<String>();
-			while (scan.hasNextLine()) {
+			while ( scan.hasNextLine() ) {
 				String eachRow = scan.nextLine();
 
-				String[] columns = eachRow.split(",");
+				String[] columns = eachRow.split( "," );
 
-				// --------------------
-				// save column title
-				// --------------------
+				// skip title
 				lineCnt += 1;
-				if (lineCnt == 1) {
-					title = new ArrayList<String>(Arrays.asList(columns));
+				if ( lineCnt == 1 ) {
+					this.title = Arrays.asList( columns );
 					continue;
 				}
 
-				// -------------------------------
-				// save each row data to HashMap
-				// -------------------------------
-				Map<String, String> row = new HashMap<String, String>();
-
-				for (int i = 0; i < columns.length; i++) {
-					row.put(title.get(i), columns[i]);
-				}
+				// save each row data to ArrayList
+				cars.add( new Car( columns[ 0 ], columns[ 1 ], Float.parseFloat( columns[ 2 ] ), Float.parseFloat( columns[ 3 ] ) ) );
 			}
 
 			scan.close();
-
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
+		} catch ( FileNotFoundException e ) {
 			e.printStackTrace();
 		}
+
+		this.cars = cars;
 	}
 
+	public List<Car> getCars() {
+		return cars;
+	}
+
+	public List<String> getTitle() {
+		return title;
+	}
 }
